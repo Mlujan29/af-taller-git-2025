7edb6371a82ca4622aadeeda1dadd6549290c9bc
Lujan Ramirez <rmlujan29@gmail.com>
2025-11-12 14:16:16 -0300

    Se añadió la función Stock y comentarios

--- desafio-01/caja.py
+++ desafio-01/caja.py
@@ -1,30 +1,50 @@
 #Implementa un CLI que lea comandos desde stdin (uno por línea) hasta recibir FIN. El sistema debe mantener 
 # un inventario en memoria y un registro de ventas.
 
+#       FUNCIONES
+
 def alta(cod,descripcion,precio,productos):
     productos[cod] = {"descripcion": descripcion, "precio": precio, "stock": 0}
     print(f"OK: ALTA {productos}")
 
 
+def stock(cod, cantidad, productos):
+    if cod in productos:                            #verifica que el codigo ingresado exista
+        productos[cod]["stock"] += cantidad
+        if cantidad > 0:
+            print(f"OK: STOCK {cod} --> +{cantidad}")
+        else:
+            print(f"OK: STOCK {cod} --> -{cantidad}")
+    else:
+        print("ERROR: El código ingresado no existe")
+
+
+#       MAIN
 productos = {}
 
 print("-------------------Caja de Kiosco-------------------")
 while True:
     print("Acciones: ALTA, STOCK, VENDE, DEVUELVE, REPORTE, FIN")
     entrada = input("Realizar: ")
-    if entrada.strip():
-        partes = entrada.split()
-        match partes[0].upper():
+    if entrada.strip():                 #Esta funcion sirve para ver si la entrada esta vacia o solo tiene espacios
+        partes = entrada.split()        #Guardo en una lista los comandos ingresados por el usuario
+
+        match partes[0].upper():    #comparamos el comando con cada caso
             case "ALTA":
-                if len(partes) == 4:
+                if len(partes) == 4:    #validación en caso de que no se ingrese en el formato correcto
                     cod = partes[1]
                     descripcion = partes[2]
                     precio =  partes[3]
-                    alta(cod,descripcion,precio, productos)
+                    alta(cod,descripcion,precio,productos)
                 else:
                     print("ERROR: Ingrese el formato correcto para dar alta al producto")
             case "STOCK":
-                print("funcion")
+                if len(partes) == 3:    #validación en caso de que no se ingrese en el formato correcto
+                    cod = partes[1]
+                    cantidad = int(partes[2])
+                    stock(cod,cantidad,productos)
+                else:
+                    print("ERROR: Ingrese el formato correcto para dar alta al producto")
             case "VENDE":
                 print("aca va la funcion")
             case "DEVUELVE":
