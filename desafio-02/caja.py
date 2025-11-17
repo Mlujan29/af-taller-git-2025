d26c3f4f05f1fd9fe8c864ea31a56902cbe74144
Lujan Ramirez <rmlujan29@gmail.com>
2025-11-17 11:36:21 -0300

    Creación de las funciones: stock y promos_activas

--- desafio-02/caja.py
+++ desafio-02/caja.py
@@ -15,6 +15,23 @@ def alta(cod,descripcion,precio,productos):
         print(f"OK: ALTA {cod} - {descripcion}")
 
 
+def stock(cod, cantidad, productos):
+    if cod in productos:                            #verifica que el codigo ingresado exista
+        productos[cod]["stock"] += cantidad
+        if cantidad > 0:
+            print(f"OK: STOCK {cod} --> +{cantidad}")
+        else:
+            print(f"OK: STOCK {cod} --> {cantidad}")
+    else:
+        print("ERROR: El código ingresado no existe")
+
+
+def promos_activas():               # muestra las promociones
+    print("""PROMOCIONES ACTIVAS:
+    1) 3x2 en chicle
+    2) 10% en agua500 desde 5 unidades
+    3) Descuento de 1000 Gs en venta conjunta de galleta y cafe""")
+
 
 #   MAIN
 productos =  {}     # Donde se van a almacenar los productos
@@ -64,7 +81,17 @@ while True:
                     print("ERROR: Ingrese el formato correcto para dar alta al producto")
             
             case "STOCK":
-                print("la funcion")
+                if len(partes) == 3:    #validación en caso de que no se ingrese en el formato correcto
+                    cod = partes[1]
+                    
+                    try:
+                        cantidad = int(partes[2])       #la cantidad si puede ser negativa, en caso de que algo le pase al stock ya definido
+                        stock(cod,cantidad,productos)
+                    except ValueError:
+                        print("ERROR: La cantidad debe de ser un número")
+                
+                else:
+                    print("ERROR: Ingrese el formato correcto para dar stock al producto")
 
             case "VENDE":
                 print("la funcion")
@@ -73,7 +100,7 @@ while True:
                 print("la funcion")
 
             case "PROMOS":
-                print("la funcion")
+                promos_activas()
 
             case "REPORTE":
                 print("la funcion")
